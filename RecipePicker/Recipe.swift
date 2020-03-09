e043b9305c440d959ced81c69054ed5e0fc95057
Muhaimie Mazlah <laws_basic@yahoo.co.uk>
2020-03-09 15:49:01 +0900

    xml coffin

---
+++ RecipePicker/Recipe.swift
@@ -0,0 +1,102 @@
+//
+//  Recipe.swift
+//  RecipePicker
+//
+//  Created by Muhaimie Mazlah on 09/03/2020.
+//  Copyright © 2020 Muhaimie Mazlah. All rights reserved.
+//
+
+import Foundation
+
+struct RecipeType{
+    var name : String
+}
+
+class RecipeParser : NSObject{
+    
+    var xmlParser : XMLParser?
+    var recipeName : [RecipeType] = []
+    var xmlText = ""
+    var currentRecipe: RecipeType?
+    
+    init(withXML xml : String) {
+        if let data = xml.data(using: .utf8){
+            xmlParser = XMLParser(data : data)
+        }
+    }
+    
+    func parse()->[RecipeType]{
+        xmlParser?.delegate = self
+        xmlParser?.parse()
+        return recipeName
+    }
+}
+
+
+extension RecipeParser : XMLParserDelegate{
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        //for every new element reset the text
+
+        xmlText = ""
+        if elementName == "recipe"{
+            currentRecipe = RecipeType(name: "")
+        }
+    }
+    
+    //when reach the end node
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        if elementName == "name"{
+           
+            currentRecipe?.name = xmlText.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        }
+        
+        //no need '/'
+        if elementName == "recipe"{
+            if let recipe = currentRecipe{
+                recipeName.append(recipe)
+            }
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        xmlText += string
+    }
+    
+    
+    public static func parserCall()->[String]{
+        
+        var recipes = [String]()
+        
+        do{
+            if let xmlUrl = Bundle.main.url(forResource: "recipetypes", withExtension: "xml"){
+                let xml = try String(contentsOf: xmlUrl)
+                let gameParser = RecipeParser(withXML: xml)
+                let recipesType = gameParser.parse()
+                
+                for recipe in recipesType{
+                    recipes.append(recipe.name)
+                }
+            }else{
+                
+                print("No available xml file")
+                
+            }
+        }catch{
+            print(error)
+        }
+        
+        return recipes
+        
+    }
+
+        
+    
+
+}
+
+
+
+
